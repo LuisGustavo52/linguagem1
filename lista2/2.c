acefddd5fb430f8f1426897117b9a726b226c5ea
Luís Gustavo Teodoro da Silva <142850863+LuisGustavo52@users.noreply.github.com>
2023-09-12 22:12:14 -0300

    Add files via upload

--- lista2/2.c
+++ lista2/2.c
@@ -2,27 +2,35 @@
 #include <stdlib.h>
 
 int main(){
-	int matriz[100][100], tamanhoL, tamanhoC, i,j, maior = 0;
+	int matriz[100][100], tamanhoL, tamanhoC, i,j, menor;
 	
 	printf("Digite o tamanho da matriz:");
 	scanf("%d %d",&tamanhoL,&tamanhoC);
-	
+	printf("roberto");
 	for(i=0; i<tamanhoL;i++){
 		for(j=0; j<tamanhoC;j++){
+			printf("alberto");
 			scanf("%d",&matriz[i][j]);
+			
 		}
 	}
 	
 	
 	for(i=0; i<tamanhoL;i++){
 		for(j=0; j<tamanhoC;j++){
-			if(matriz[i][j] > maior){
-				maior = matriz[i][j];
+			if(i==0 && j==0){
+				menor = matriz[i][j];
+			}else{
+				if(matriz[i][j] < menor){
+				menor = matriz[i][j];
+				}
 			}
 		}
-		
+			
 	}
 	
-	printf("O maior valor e: %d",maior);
+	printf("O menor valor e: %d",menor);
+	
+	return 0;
 	
 }
