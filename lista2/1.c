9b471ec8f5fb385107fe7f3a20aa8b749aaf1df5
Luís Gustavo Teodoro da Silva <142850863+LuisGustavo52@users.noreply.github.com>
2023-09-12 21:51:09 -0300

    Update 1.c

--- lista2/1.c
+++ lista2/1.c
@@ -1,1 +1,27 @@
-a
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int matriz[100][100], tamanhoL, tamanhoC, i,j, maior = 0;
+	
+	printf("Digite o tamanho da matriz:");
+	scanf("%d%d",&tamanhoL,&tamanhoC);
+	
+	for(i=0; i<tamanhoL;i++){
+		for(j=0; j<tamanhoC;j++){
+			scanf("%d\n0",&matriz[i][j]);
+		}
+	}
+	
+	
+	for(i=0; i<tamanhoL;i++){
+		for(j=0; j<tamanhoC;j++){
+			if(matriz[i][j] > maior){
+				maior = matriz[i][j];
+			}
+		}
+	}
+	
+	printf("O maior valor e: %d",maior);
+	
+}
